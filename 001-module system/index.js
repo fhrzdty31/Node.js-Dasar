90961e27115413e2dbb34d6e51c11f3ae2df3df4
FahrezaDityaAlfandry <fahreza_ditya_29rpl@student.smktelkom-mlg.sch.id>
2022-07-28 16:08:33 +0700

    Node.js Dasar

--- 001-module system/index.js
+++ 001-module system/index.js
@@ -3,6 +3,6 @@ const coba = require('./coba');
 console.log(
     coba.getData.vData(),
     coba.sekolah('SDN Setonorejo 2', 'SD'),
-    coba.sekolah('SMP Negri 1 Ngadiluwih', 'SMP'),
+    coba.sekolah('SMP Negeri 1 Ngadiluwih', 'SMP'),
     coba.sekolah('SMK Telkom Malang', 'SMA')
 );
